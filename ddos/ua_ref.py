31772224a973da76fd16ea8c5ee5f677920fca2c
Just Rey? <89574993+fooster1337@users.noreply.github.com>
2023-10-08 20:26:03 +0700

    Update ua_ref.py

--- ddos/ua_ref.py
+++ ddos/ua_ref.py
@@ -5,7 +5,7 @@
 import random
 
 def user_agent() -> str:
-    return random.choice(open('lib/ua.txt', 'r').read().splitlines())
+    return random.choice(open('ddos/lib/ua.txt', 'r').read().splitlines())
     
 def referer() -> str:
-    return random.choice(open('lib/referer.txt', 'r').read().splitlines())
+    return random.choice(open('ddos/lib/referer.txt', 'r').read().splitlines())
